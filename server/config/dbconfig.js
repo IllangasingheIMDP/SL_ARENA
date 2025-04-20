6b4aac82d7de046c7c0f7921652f9484189f0d22
Dasun Illangasinghe <157776370+IllangasingheIMDP@users.noreply.github.com>
2025-04-21 01:01:25 +0530

    backend

---
+++ server/config/dbconfig.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const mysql = require('mysql2');
+const fs = require('fs');
+
+// Read the SSL certificate
+const ca = fs.readFileSync(process.env.SSL_CA_PATH);
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  
+  ssl: {
+    ca: ca
+  },
+
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+module.exports = pool.promise();
---
+++ server/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const db = require('./config/dbconfig');
+const cors = require('cors');
+
+const allowedOrigins = [
+  'http://localhost:5173',
+  'http://localhost:5174',
+  'http://16.171.12.238',
+  'https://lpedu.lk'
+];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, origin);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
+
+app.use(express.json());
+
+// ✅ DB Connection Check
+db.query('SELECT 1')
+  .then(() => {
+    console.log('✅ Database connected successfully.');
+  })
+  .catch((err) => {
+    console.error('❌ Database connection failed:', err.message);
+  });
+
+app.listen(3000, () => {
+  console.log('🚀 Server running on http://localhost:3000');
+});
