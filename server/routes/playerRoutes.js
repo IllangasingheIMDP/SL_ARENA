fbba1fac35b9639930e62a905201f8aa7b89c49e
Kasun333 <kumarajwkd.22@uom.lk>
2025-04-23 01:48:44 +0530

    player backend

---
+++ server/routes/playerRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const playerController = require('../controllers/playerController');
+const { authenticateToken, checkRole } = require('../middleware/auth');
+
+
+router.get('/stat', authenticateToken,checkRole(['player']), playerController.getPlayerStats);
+router.get('/achievements', authenticateToken,checkRole(['player']), playerController.fetchPlayerAchievements);
+router.get('/performance', authenticateToken, checkRole(['player']),playerController.fetchPerformanceTrend);
+
+
+
+
+
+
+module.exports = router;
