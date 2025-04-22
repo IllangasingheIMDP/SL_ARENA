8e6a5544ccb2910f4d7b6fdba36c5b23b623c081
Dasun Illangasinghe <157776370+IllangasingheIMDP@users.noreply.github.com>
2025-04-23 01:44:29 +0530

    add

--- slarena/babel.config.js
+++ slarena/babel.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: [
+    'module:metro-react-native-babel-preset',
+     // add this if you’re using TypeScript
+           // add this only if you're using Flow
+  ],
   plugins: [
     [
       'module:react-native-dotenv',
