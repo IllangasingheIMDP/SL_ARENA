0e29a7f6e8f7943756a51f0930d71f42e8900749
Dasun Illangasinghe <157776370+IllangasingheIMDP@users.noreply.github.com>
2025-04-23 03:49:25 +0530

    working mobile app test

--- slarena/babel.config.js
+++ slarena/babel.config.js
@@ -1,16 +1,3 @@
 module.exports = {
-  presets: [
-    'module:metro-react-native-babel-preset',
-     // add this if you’re using TypeScript
-           // add this only if you're using Flow
-  ],
-  plugins: [
-    [
-      'module:react-native-dotenv',
-      {
-        moduleName: '@env',
-        path: '.env',
-      },
-    ],
-  ],
+  presets: ['module:metro-react-native-babel-preset'],
 };
