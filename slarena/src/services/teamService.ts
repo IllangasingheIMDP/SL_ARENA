a3a9c28f504d8f095d41ecb4d241193aed96b441
Dasun Illangasinghe <157776370+IllangasingheIMDP@users.noreply.github.com>
2025-04-25 13:30:23 +0530

    Update teamService.ts

--- slarena/src/services/teamService.ts
+++ slarena/src/services/teamService.ts
@@ -12,37 +12,37 @@ import {
 
 const getMyTeams = async (): Promise<Team[]> => {
     const response = await api.get('/teams/my-teams');
-    console.log(response.data,'response in getMyTeams');
+    //console.log(response.data,'response in getMyTeams');
     return response.data;
 };
 const getTeamsLedByMe = async (): Promise<Team[]> => {
     const response = await api.get('/teams/teams-led-by-player');
-    console.log(response.data,'response in getTeamsLedByMe');
+    //console.log(response.data,'response in getTeamsLedByMe');
     return response.data;
 };
 
 
 const getPlayerTeams = async (userId: number): Promise<Team[]> => {
     const response = await api.get(`/teams/player/${userId}/teams`);
-    console.log(response.data,'response in getPlayerTeams');
+    //console.log(response.data,'response in getPlayerTeams');
     return response.data;
 };
 
 const getAllTeams = async (): Promise<Team[]> => {
     const response = await api.get('/teams');
-    console.log(response.data,'response in getAllTeams');
+    //console.log(response.data,'response in getAllTeams');
     return response.data;
 };
 
 const getTeamPlayers = async (teamId: number): Promise<TeamPlayer[]> => {
     const response = await api.get(`/teams/${teamId}/players`);
-    console.log(response.data,'response in getTeamPlayers');
+    //console.log(response.data,'response in getTeamPlayers');
     return response.data;
 };
 
 const getTeamByName = async (teamName: string): Promise<Team[]> => {
     const response = await api.get(`/teams/team-by-name/${teamName}`);
-    console.log(response.data,'response in getTeamByName');
+    //console.log(response.data,'response in getTeamByName');
     return response.data;
 };
 
